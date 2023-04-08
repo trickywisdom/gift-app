f5592db6137586fd0319c16a3c59c616193387fd
Spyros Trimis <spyrostrimis@gmail.com>
2023-04-08 09:44:45 +0300

    useRef instead of useState in UserForm

--- client/src/components/UserForm.js
+++ client/src/components/UserForm.js
@@ -1,44 +1,34 @@
-import { useState } from 'react';
+import { useRef } from "react";
 import axios from "axios";
 
 function UserForm() {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
+  const nameRef = useRef();
+  const emailRef = useRef();
 
-    function createNewUser(e) {
-        e.preventDefault();
-        let newUser = {
-            name: name,
-            email: email,
-        };
-        axios
-          .post("https://gift-app-shxz.onrender.com/user/create", newUser)
-          .then((res) => alert(res.data.msg));
-    }
+  function createNewUser() {
+    e.preventDefault();
+    let newUser = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+    };
+    axios
+      .post("https://gift-app-shxz.onrender.com/user/create", newUser)
+      .then((res) => alert(res.data.msg));
+  }
 
-    return (
-      <form onSubmit={createNewUser}>
-        <label htmlFor="name">Full Name: </label>
-        <input
-          id="name"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <br></br>
-        <br></br>
-        <label htmlFor="email">Email: </label>
-        <input
-          id="email"
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-            <br />
-            <button type='submit'>Submit</button>
-      </form>
-    );
+  return (
+    <form onSubmit={createNewUser}>
+      <label htmlFor="name">Full Name: </label>
+      <input id="name" type="text" ref={nameRef} />
+      <br></br>
+      <br></br>
+      <label htmlFor="email">Email: </label>
+      <input id="email" type="text" ref={emailRef} />
+      <br />
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default UserForm;
